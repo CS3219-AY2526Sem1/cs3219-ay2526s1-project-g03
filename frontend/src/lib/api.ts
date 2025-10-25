5213cc364d201ed068f57f6171a3acd8fb22711c
ruiming97 <e1130094@u.nus.edu>
2025-10-25 20:23:40 +0800

    fix(frontend): amend to previous commit

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,44 +1,46 @@
-import API from '../config/apiClient';
+import { userApi, matchingApi } from '../config/apiClient';
 
-export const register = async data => API.post('/auth/register', data);
+export const register = async data => userApi.post('/auth/register', data);
 
-export const login = async data => API.post('auth/login', data);
-export const logout = async () => API.get('auth/logout');
+export const login = async data => userApi.post('auth/login', data);
+export const logout = async () => userApi.get('auth/logout');
 
 export const verifyEmail = async verificationCode =>
-  API.get(`/auth/email/verify/${verificationCode}`);
-export const resendEmail = async data => API.post('auth/email/resend', data);
+  userApi.get(`/auth/email/verify/${verificationCode}`);
+export const resendEmail = async data => userApi.post('auth/email/resend', data);
 
-export const forgotPassword = async data => API.post('/auth/password/forgot', data);
-export const resetPassword = async data => API.post('/auth/password/reset', data);
+export const forgotPassword = async data => userApi.post('/auth/password/forgot', data);
+export const resetPassword = async data => userApi.post('/auth/password/reset', data);
 
 export const getUser = async () => {
-  const response = await API.get('/user', {withCredentials: true});
+  const response = await userApi.get('/user', {withCredentials: true});
   return response.data;
 };
-export const changeUsernameOrEmail = async data => API.patch('/user/profile/usernameoremail', data);
+export const changeUsernameOrEmail = async data => userApi.patch('/user/profile/usernameoremail', data);
 export const changeProfilePic = async (formData: FormData) =>
-  API.patch('/user/profile/picture', formData, {
+  userApi.patch('/user/profile/picture', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   }).then(res => res.data);
 
 export const deleteProfilePic = async () => {
-  return API.patch('/user/profile/picture', {delete: 'true'});
+  return userApi.patch('/user/profile/picture', {delete: 'true'});
 };
 
-export const changePassword = async data => API.patch('/user/profile/password', data);
-export const changePersonalInfo = async data => API.patch('/user/profile/personalinfo', data);
+export const changePassword = async data => userApi.patch('/user/profile/password', data);
+export const changePersonalInfo = async data => userApi.patch('/user/profile/personalinfo', data);
 
-export const deleteAccount = async (data: {password: string}) => API.delete('/user/delete', {data});
+export const deleteAccount = async (data: {password: string}) => userApi.delete('/user/delete', {data});
 
 export const unlinkOAuthProvider = async (provider: 'google' | 'github') =>
-  API.delete(`/user/oauth/${provider}`);
+  userApi.delete(`/user/oauth/${provider}`);
 
 export const changeUserRole = async (username, role) =>
-  API.patch(`/admin/users/${username}/role`, {role});
+  userApi.patch(`/admin/users/${username}/role`, {role});
 
-export const createAdminAccount = async data => API.post('/admin/users', data);
+export const createAdminAccount = async data => userApi.post('/admin/users', data);
 
-export const findMatch = async data => API.post('/api/matches/', data);
+
+// matching-service
+export const findMatch = async data => matchingApi.post('/matches/', data);
