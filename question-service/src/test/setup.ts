447f0cf3f585d58ceaea86a72c8b648a61f8326f
verakohh <vrkwjacad@gmail.com>
2025-10-30 15:05:11 +0800

    fix setup for tests

--- question-service/src/test/setup.ts
+++ question-service/src/test/setup.ts
@@ -1,13 +1,1 @@
-process.env['NODE_ENV'] = 'test';
-
-// Mock the required database environment variables
-// Use the same values as your .env.test file
-process.env['POSTGRES_HOST'] = 'question-db-pg'; 
-process.env['POSTGRES_PORT'] = '5432';
-process.env['POSTGRES_USER'] = 'postgres';
-process.env['POSTGRES_PASSWORD'] = 'postgres';
-process.env['POSTGRES_DB'] = 'question-db';
-
-
-console.log('Jest setup: Environment variables mocked for test environment.');
-
+console.log('Jest setupFilesAfterEnv: Running setup.ts...');
